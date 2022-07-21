506db7ab25b5f096d25e5be4264c439a3110adfd
webersilv4 <webercost4@hotmail.com>
2022-07-21 18:58:13 -0300

    Create Dashboard and Routers

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -4,12 +4,12 @@ import { api } from '../../Utils/Api/api';
 
 const Dashboard = () => {
 
-    const [response, setResponse] = useState();
+    const [response, setResponse] = useState([]);
     const [error, setError] = useState();
 
     const fetchData = async () => {
         await api.get('/render-urls')
-            .then(response => { console.log(response); setResponse([response.data]); })
+            .then(response => { setResponse(response.data); })
             .catch(error => setError(error.response.data.error) );
     };
 
@@ -19,8 +19,25 @@ const Dashboard = () => {
 
     return (
         <div className="App">
-            <h1>Ola</h1>
-            {error ? error: ''}
+            <table border={1}>
+                <tr>
+                    <th>URL</th>
+                    <th>STATUS</th>
+                    <th>HTML RETORNADO</th>
+                    <th>ALTERAR</th>
+                    <th>DELETAR</th>
+                </tr>
+                {response.map((element, key)=>(
+                    <tr key={key}>
+                        <td>{element.url_website}</td>
+                        <td>{element.status_code == 0 ? 'Em Analise' : element.status_code}</td>
+                        <td>{element.requisition_body == '' ? 'Pendente' : element.requisition_body}</td>
+                        <td><button>Alterar</button></td>
+                        <td><button>X</button></td>
+                    </tr>
+                ))}
+                {error ? error: ''}
+            </table>
         </div>
     );
 };
